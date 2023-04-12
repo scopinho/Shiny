2be810439565338ed4152f867c9c2a135c610ce2
scopinho <eric.scopinho@gmail.com>
2023-04-12 13:33:17 -0300

    all files from folder

---
+++ app.R
@@ -0,0 +1,23 @@
+library(shiny)
+
+ui <- fluidPage(
+  selectInput("dataset", label="Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table")
+)
+
+server <- function(input, output) {
+  output$summary <- renderPrint({
+    dataset <- get(input$dataset, "package:datasets")
+    summary(dataset)
+  }
+  )
+  output$table <- renderTable({
+    dataset <- get(input$dataset, "package:datasets")
+    dataset
+  }
+  )
+}
+
+
+shinyApp(ui = ui, server = server)
